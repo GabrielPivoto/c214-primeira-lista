03e177995081fe22a5888b9473de29d6766be289
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2021-08-30 10:54:47 -0300

    Main atualizada

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -7,9 +7,14 @@ public class Principal {
         GameReader gr = new GameReader();
 
         List<Game> gamesPS4 = gr.listarPorPlataforma("PS4");
+        List<Game> gamesSony = gr.listarPorPublisher("Sony Computer Entertainment");
 
         for(int i = 0; i < gamesPS4.size(); i++){
             System.out.println(gamesPS4.get(i).getName());
+        }
+        System.out.println("");
+        for(int i = 0; i < gamesSony.size(); i++){
+            System.out.println(gamesSony.get(i).getName());
         }
 
     }
