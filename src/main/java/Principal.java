407981ba15ff319566e4bbee8636dd5186d99c7c
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2021-10-12 14:28:50 -0300

    Classe principal atualizada

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,21 +1,31 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Principal {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         GameReader gr = new GameReader();
 
-        List<Game> gamesPS4 = gr.listarPorPlataforma("PS4");
-        List<Game> gamesSony = gr.listarPorPublisher("Sony Computer Entertainment");
+        System.out.println("Qual plataforma deseja buscar?");
+        String plataforma = sc.nextLine();
+        System.out.println("Qual publisher deseja buscar?");
+        String publisher = sc.nextLine();
 
-        for(int i = 0; i < gamesPS4.size(); i++){
-            System.out.println(gamesPS4.get(i).getName());
-        }
-        System.out.println("");
-        for(int i = 0; i < gamesSony.size(); i++){
-            System.out.println(gamesSony.get(i).getName());
-        }
+        List<Game> gamesPlataforma = gr.listarPorPlataforma(plataforma);
+        List<Game> gamesPublisher = gr.listarPorPublisher(publisher);
+
+        System.out.println("Jogos da plataforma " + plataforma + ":");
+        for(int i = 0; i < gamesPlataforma.size(); i++)
+            System.out.println(gamesPlataforma.get(i).getName());
+
+        System.out.println(" ");
+
+        System.out.println("Jogos do publisher " + publisher + ":");
+        for(int i = 0; i < gamesPublisher.size(); i++)
+            System.out.println(gamesPublisher.get(i).getName());
+
 
     }
 
